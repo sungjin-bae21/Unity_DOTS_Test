09cba417afc279db471dd65c289b7c0146350856
Sungjin Bae <sungjin.bae21@gmail.com>
2021-02-13 17:39:50 +0900

    Add Layer Util

--- Assets/Script/Player/ClientMouseInputSystem.cs
+++ Assets/Script/Player/ClientMouseInputSystem.cs
@@ -32,16 +32,15 @@ public class ClientMouseInputSystem : ComponentSystem
         }
 
         var screenPointToRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        LayerMask ground = LayerMask.GetMask("Default");
+        LayerMask mouse_ray = LayerMask.GetMask("MouseRay");
+        CollisionFilter collision_filter = LayerUtil.LayerMaskToFilter(mouse_ray, ground);
         var raycast_input = new RaycastInput
         {
             Start = screenPointToRay.origin,
             End = screenPointToRay.origin + (screenPointToRay.direction * RAYCAST_DISTANCE),
-            /*Filter = new CollisionFilter
-            {
-                BelongsTo = 1u << 6,
-                CollidesWith = 1u << 3,
-            }*/
-            Filter = CollisionFilter.Default
+            Filter = collision_filter
         };
 
         if (!physicsWorld.PhysicsWorld.CollisionWorld.CastRay(raycast_input, out var hit))
@@ -56,6 +55,9 @@ public class ClientMouseInputSystem : ComponentSystem
 
         var req = PostUpdateCommands.CreateEntity();
         PostUpdateCommands.AddComponent(req, input);
+
+        // 타겟을 추가해야 할거같은데;; 예상으로 묵시적으로 서버에 전송될듯?
+        // 나중에 추가하자
         PostUpdateCommands.AddComponent(req, new SendRpcCommandRequestComponent());
     }
 }
---
+++ Assets/Script/Util/LayerUtil.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using Unity.Physics;
+using UnityEngine.Rendering;
+
+public static class LayerUtil
+{
+
+    public static CollisionFilter LayerMaskToFilter(LayerMask belong_to, LayerMask collision_with)
+    {
+        CollisionFilter filter = new CollisionFilter()
+        {
+            BelongsTo = (uint)belong_to.value,
+            CollidesWith = (uint)collision_with.value
+        };
+        return filter;
+    }
+
+}
